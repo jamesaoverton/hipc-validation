5c3f2047a276c81a6ccb5ee1a5dd7cab27cbda65
Michael Cuffaro <mike@gwydion>
2019-05-27 13:05:52 +0200

    WIP use separate API calls for HAI and neutAbTiter

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@
 validate: validate.py nodes.dmp names.dmp sample.xlsx
 	$^ result.xlsx
 
+hai.csv neutAbTiter.csv: validate_studies.py nodes.dmp names.dmp
+	$< --nodes nodes.dmp --names names.dmp \
+	--hai SDY113 SDY144 SDY180 SDY202 SDY212 SDY312 SDY387 SDY404 SDY514 SDY515 SDY519 SDY67 \
+	--neutAbTiter SDY144 SDY180 SDY387 SDY522 SDY67
+
 %.dmp: taxdmp.zip
 	unzip -u $<
 
@@ -10,7 +15,7 @@ taxdmp.zip:
 	curl -k -L -o $@ "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip"
 
 clean:
-	rm -f taxdmp.zip *.dmp gc.prt readme.txt
+	rm -f taxdmp.zip *.dmp gc.prt readme.txt *.csv
 
 cleanall: clean
 	rm -Rf static
--- validate_studies.py
+++ validate_studies.py
@@ -2,9 +2,11 @@
 
 import argparse
 import getpass
+import os.path
 import re
 import requests
 import sys
+import time
 
 
 def extract_nodes(nodes_file):
@@ -22,7 +24,7 @@ def extract_nodes(nodes_file):
 def extract_names(names_file):
   """
   Given the NCBI names.dmp file handle, return four dictionaries:
-  taxid_names, scientific_names, synonyms, and lowercase_names
+  `taxid_names`, `scientific_names`, `synonyms`, and `lowercase_names`
   """
   taxid_names = {}
   scientific_names = {}
@@ -40,7 +42,8 @@ def extract_names(names_file):
   return taxid_names, scientific_names, synonyms, lowercase_names
 
 
-def determine_preferred(reported, taxid_names, scientific_names, synonyms, lowercase_names):
+def determine_preferred(reported, parents, taxid_names, scientific_names, synonyms,
+                        lowercase_names):
   taxid = None
   scientific_name = None
   automatic_replacement = False
@@ -76,7 +79,9 @@ def determine_preferred(reported, taxid_names, scientific_names, synonyms, lower
         taxid = scientific_names[scientific_name]
 
   def is_virus(taxid):
-    """Given a taxonomy ID, return true if it is a virus, false otherwise."""
+    """
+    Given a taxonomy ID, return true if it is a virus, false otherwise.
+    """
     return taxid and taxid != '1' and (taxid == '10239' or is_virus(parents[taxid]))
 
   preferred = None
@@ -105,40 +110,62 @@ def write_record(record, headers, outfile, parents, taxid_names,
   for header in headers:
     print('"{}",'.format(record[header]), end='', file=outfile)
 
-  preferred, comment = determine_preferred(record['unitReported'], taxid_names, scientific_names,
-                                           synonyms, lowercase_names)
+  preferred, comment = determine_preferred(record['virusStrainReported'], parents, taxid_names,
+                                           scientific_names, synonyms, lowercase_names)
   print('"{}","{}",'.format(preferred, comment), end='', file=outfile)
-  print("Y", file=outfile) if record['unitPreferred'] == preferred else print("N", file=outfile)
+  print("Y", file=outfile) if record['virusStrainPreferred'] == preferred else print("N", file=outfile)
 
 
 def main():
+  # Basic command-line arguments:
   parser = argparse.ArgumentParser(description='Validate studies submitted to ImmPort')
-  parser.add_argument('--output', metavar='CSV', type=argparse.FileType('w'),
-                      help='The output CSV file (or STDOUT if unspecified)')
   parser.add_argument('--username', metavar='USERNAME', type=str,
                       help='username for authentication to ImmPort API')
   parser.add_argument('--password', metavar='PASSWORD', type=str,
                       help='password for authentication to ImmPort API')
-  parser.add_argument('nodes', metavar='NODES', type=argparse.FileType('r'),
+  parser.add_argument('--nodes', metavar='NODES', type=argparse.FileType('r'), required=True,
                       help='The NCBI nodes.dmp file')
-  parser.add_argument('names', metavar='NAMES', type=argparse.FileType('r'),
+  parser.add_argument('--names', metavar='NAMES', type=argparse.FileType('r'), required=True,
                       help='The NCBI names.dmp file')
-  parser.add_argument('study_ids', metavar='STUDY_ID', type=str, nargs='+',
-                      help='id of study to validate')
-  args = parser.parse_args()
+  parser.add_argument('--clobber', dest='clobber', action='store_true',
+                      help='If CSV files exist, overwrite them without prompting')
+  # Command-line arguments used to specify the study ids to validate.
+  # ---
+  # For each type of study that will be validated (e.g. 'hai', 'neutAbTiter'), a separate
+  # command-line option must be specified. Whenever more study types are added, a new
+  # command-line option corresponding to that study type should be added to `study_type_group`,
+  # and the function `get_requested_endpoints()` should be modified accordingly.
+  def get_requested_endpoints(args):
+    endpoints = []
+    if args.get('hai'):
+      endpoints.append('hai')
+    if args.get('neutAbTiter'):
+      endpoints.append('neutAbTiter')
+    return endpoints
+
+  study_type_group = parser.add_argument_group()
+  study_type_group.add_argument('--hai', metavar='ID', type=str, nargs='+',
+                                help='ids of Hemagglutination Inhibition studies to validate')
+  study_type_group.add_argument('--neutAbTiter', metavar='ID', type=str, nargs='+',
+                                help='ids of Neutralizing Antibody Titer studies to validate')
+
+  args = vars(parser.parse_args())
+
+  endpoints = get_requested_endpoints(args)
+  if not endpoints:
+    print("At least one study type must be specified")
+    sys.exit(1)
 
   # If the username and/or password have not been specified on the command line, prompt for them:
-  username = args.username
+  username = args['username']
   if not username:
     username = input("Enter username for API calls: ")
-  password = args.password
+  password = args['password']
   if not password:
     password = getpass.getpass('Enter password for API calls: ')
 
-  # If no output file has been specified, then just use STDOUT:
-  outfile = args.output
-  if not outfile:
-    outfile = sys.stdout
+  # Get the start time of the execution for later logging the total elapsed time:
+  start = time.time()
 
   # Get the authentication token:
   resp = requests.post('https://auth.immport.org/auth/token',
@@ -148,32 +175,42 @@ def main():
   token = resp.json()['token']
 
   # Now request data for the given study ids:
-  query = ("https://api.immport.org/data/query/result/elisa?studyAccession={}"
-           .format(','.join(args.study_ids)))
-  print("Sending request: " + query, file=sys.stderr)
-  resp = requests.get(query, headers={"Authorization":"bearer " + token})
-  if resp.status_code != requests.codes.ok:
-    resp.raise_for_status()
+  parents = extract_nodes(args['nodes'])
+  taxid_names, scientific_names, synonyms, lowercase_names = extract_names(args['names'])
+  for endpoint in endpoints:
+    if os.path.exists(endpoint + '.csv') and args['clobber'] is False:
+      reply = input('{}.csv exists. Do you want really want to overwrite it? (y/n): '
+                    .format(endpoint))
+      reply = reply.lower().strip()
+      if reply == 'n':
+        continue
+    with open(endpoint + '.csv', 'w') as outfile:
+      query = ("https://api.immport.org/data/query/result/{}?studyAccession={}"
+               .format(endpoint, ','.join(args[endpoint])))
+      print("Sending request: " + query)
+      resp = requests.get(query, headers={"Authorization":"bearer " + token})
+      if resp.status_code != requests.codes.ok:
+        resp.raise_for_status()
 
-  # Write the header of the CSV using the data returned:
-  headers = sorted([key for key in resp.json()[0]])
-  for header in headers:
-    print("{},".format(header), end='', file=outfile)
-  print("Preferred (determined),", end='', file=outfile)
-  print("Comment,", end='', file=outfile)
-  print("Preferred (determined) matches unitPreferred", file=outfile)
-  
-  # Now write the actual data:
-  parents = extract_nodes(args.nodes)
-  taxid_names, scientific_names, synonyms, lowercase_names = extract_names(args.names)
-  for sid in args.study_ids:
-    records = [r for r in resp.json() if r['studyAccession'] == sid]
-    print("Received {} records for Study ID: {}".format(len(records), sid), file=sys.stderr)
-    for record in records:
-      write_record(
-        record, headers, outfile, parents, taxid_names, scientific_names, synonyms, lowercase_names)
+      # Write the header of the CSV using the data returned:
+      headers = sorted([key for key in resp.json()[0]])
+      for header in headers:
+        print("{},".format(header), end='', file=outfile)
+      print("Preferred (determined),", end='', file=outfile)
+      print("Comment,", end='', file=outfile)
+      print("Preferred (determined) matches virusStrainPreferred", file=outfile)
+
+      # Now write the actual data:
+      for sid in args[endpoint]:
+        records = [r for r in resp.json() if r['studyAccession'] == sid]
+        print("Received {} records for {} ID: {}".format(len(records), endpoint, sid))
+        for record in records:
+          write_record(
+            record, headers, outfile, parents, taxid_names, scientific_names, synonyms, lowercase_names)
+
+  end = time.time()
+  print("Processing completed. Total execution time: {0:.2f} seconds.".format(end - start))
 
 
 if __name__ == "__main__":
   main()
-
